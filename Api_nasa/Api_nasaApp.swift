c938dc572a2c6aaf9129972d9698665ddc83d213
Alexander Gnilitskiy <gnilitskiy1997@icloud.com>
2022-11-06 19:10:19 +0300

    Initial Commit

---
+++ Api_nasa/Api_nasaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Api_nasaApp.swift
+//  Api_nasa
+//
+//  Created by Александр on 06.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Api_nasaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
